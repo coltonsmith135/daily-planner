0bd07c1c2c4e219ba0fe008eeafd5f8c47f18d3a
coltonsmith135 <115854214+coltonsmith135@users.noreply.github.com>
2022-12-12 21:59:47 -0800

    added html and JavaScript code.

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,10 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var hour = $('#hour-9');
-var saveBtn = $('.btn');
+var hour = $('#hour-x');
+var saveBtn = $('saveBtn');
+
+var timeBlock = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
 
 
 
@@ -11,8 +13,7 @@ var saveBtn = $('.btn');
 
 
 
-
-$(function () {
+$(document).ready(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -33,23 +34,23 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-    if (hour < today) {
-      
+    if (timeBlock < today) {
+      hour.attr('class', 'past');
     } 
 
-    if (hour === today) {
-      
+    if (timeBlock === today) {
+      hour.attr('class', 'present');
     }
 
-    if (hour > today) {
-      
+    if (timeBlock > today) {
+      hour.attr('class', 'future');
     }
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+  // attribute of each time-block be used to do this?                                 
   
   // This displays the current date on the top of my webpage using the id "currentDate"
   var today = dayjs();
-  $('#currentDay').text(today.format('dddd, MMMM D, h:mm' + "a"));
+  $('#currentDay').text(today.format(`dddd, MMMM D, H:mma`));
   console.log(today);
 });
