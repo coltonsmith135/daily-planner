4631795e9c2aecf926f221168403d9103a3ef678
coltonsmith135 <115854214+coltonsmith135@users.noreply.github.com>
2023-01-12 22:01:28 -0800

    added a function to get the local storage, added comments, and changed text color to black

--- script.js
+++ script.js
@@ -1,40 +1,32 @@
 var saveBtn = $('.saveBtn');
 var description = $('.description')
 
-
-
-
-
-
-
-
-
 $(document).ready(function () {
 
   saveBtn.on("click", function (event) {
     event.preventDefault();
     console.log('button clicked')
 
+    // the value was selected by using the siblings method to find the sibling of the button being used to be able to get the value of the textarea
+
     let input = $(this).siblings('textarea').val()
+
+    // assigned the id as textarea to be able to store it in local storage as a keyitem
     let textArea = $(this).siblings('textarea').attr('id')
 
-
-    console.log(textArea)
-    console.log(input)
+    // passed textarea as a key item and input as the value
     localStorage.setItem(textArea, input)
 
-    for (let i = 0; i < 8; i++) {
-      var info = localStorage.getItem(textArea)
-    }
-    console.log(info)
-    description.innerHTML = info
 
 
   });
 
-  let currentHour = moment().hour()
+  // this does a for each loop to compare the time selected by the id and compares it to the current time to determine whether it is in the past, present, or future.
 
+  let currentHour = moment().hour()
   $('.description').each(function () {
+
+    // this lets us turn the id into a string to be able to compare integers to integers instead of string to integer
 
     let timeHour = parseInt($(this).attr('id'))
 
@@ -51,19 +43,23 @@ $(document).ready(function () {
     }
   })
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?                                 
+// this function loops through the local storage and gets the data and places it in the matching text area
 
-  for (let i = 0; i < 8; i++) {
-    var info = localStorage.getItem(textArea)
+  function init() {
+
+    // selects the class description and does a for each to loop through each text area
+
+    $('.description').each(function () {
+
+      // this selects the values stored in the local stoage assigned to each id. 
+
+      $(this).val(localStorage.getItem(this.id))
+   })
   }
-  console.log(info)
-  description.innerHTML = info
-
+// this gets the current day and time
 
   var today = dayjs();
   $('#currentDay').text(today.format(`dddd, MMMM D, H:mma`));
-
+  init();
 });
 
